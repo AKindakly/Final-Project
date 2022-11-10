9ea6718e91885a1b20852dda2358cdd94dd67ba3
AKindakly <Akindakly@gmail.com>
2022-11-10 01:26:41 +0100

    final project part1

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>Hello World this is the hotel website</h1>
+            <img src="./hotel1.jpg" alt="" />
+        </>
+    );
+}
+
+export default App;
