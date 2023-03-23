d911014853bdf3719ff56bade233299fc50d8d2e
Akindakly <akindakly@gmail.com>
2023-03-23 16:45:55 +0100

    update react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Main from "./components/Main/Main";
 import AboutUs from "./components/AboutUs/AboutUs";
@@ -9,13 +9,13 @@ import Contact from "./components/Contact/Contact";
 
 const App = () => {
     return (
-        <Switch>
-            <Route path="/" exact component={Main} />
-            <Route path="/about" exact component={AboutUs} />
-            <Route path="/rooms" exact component={Rooms} />
-            <Route path="/rooms/:id" exact component={OneRoom} />
-            <Route path="/contact" exact component={Contact} />
-        </Switch>
+        <Routes>
+            <Route path="/" exact element={<Main />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/rooms/:id" element={<OneRoom />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
     );
 };
 
